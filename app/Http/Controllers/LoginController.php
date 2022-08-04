1b3bb64a8c3fefda966b0ef427184dcac3a81263
bmgcosta <bmgcosta@outlook.com>
2022-08-04 17:23:50 +0100

    PROJFINAL-8 Add middleware sactun and config it to protect API

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
@@ -11,8 +15,35 @@ class LoginController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function login(Request $request)
     {
-        dd('Login');
+        try {
+            $validator = \Validator::make($request->all(), [
+                'email' => 'required',
+                'password' => 'required',
+            ]);
+    
+            if ($validator->fails()) {
+                $responseArr['message'] = $validator->errors()->first();
+                return response()->json($responseArr, 500);
+            }
+            $user = $this->verifUser($request);
+
+
+            return response()->json($user, 200);
+        } catch (\Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()], $exception->getCode()) ;
+        }
+        //Link: https://laravel.com/docs/7.x/validation
+        
+    }
+
+    private function verifUser(Request $request){
+        if((!Auth::attempt($request->only(['email', 'password']))))
+            throw new \Exception('User not found, please verify your email address and password!', 404);
+        $user = User::where('email', $request->email)->first();
+        if(!$user->active)
+            throw new \Exception('User is disabled, please contact administrator or your manager!', 404);
+        return $user; 
     }
 }
