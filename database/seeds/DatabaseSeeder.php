e07a469fbdedfebca8a28932797a310c6ec322ed
bmgcosta <bmgcosta@outlook.com>
2022-08-04 15:22:55 +0100

    PROJFINAL-8 Make Feature implementation (Migration & Seeder)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(UserSeeder::class);
         $this->call(RoleSeeder::class);
         $this->call(RouteSeeder::class);
+        $this->call(FeatureSeeder::class);
+        
     }
 }
---
+++ database/seeds/FeatureSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Feature;
+
+class FeatureSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $features = ['tools', 'projects', 'inspections', 'reparations', 'users'];
+        $featuresCount = count($features);
+
+        for ($i = 0; $i < $featuresCount; $i++) {
+            Feature::insert([
+                'name' => $features[$i],
+                'created_at' => now()
+            ]);
+        }
+
+    }
+}
