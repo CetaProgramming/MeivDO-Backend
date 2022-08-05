fa4e9002f4cf9679e08979d37f0636c9ea22f27f
bmgcosta <bmgcosta@outlook.com>
2022-08-05 11:32:39 +0100

    PROJFINAL-8 Add token return to user and for each user login the will be revoked and generate new token

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -29,7 +29,7 @@ class LoginController extends Controller
             }
 
             $userLogin = $this->verifUser($request);
-
+            
             return response()->json([
                 $userLogin->data(),
             ], 200);
--- app/Login.php
+++ app/Login.php
@@ -4,9 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Role;
+use Laravel\Sanctum\HasApiTokens;
 
 class Login extends User
 {
+    use HasApiTokens;
+
     protected $table = 'users';
     protected $hidden = ['role_id', 'password', 'active', 'user_id', 'created_at', 'updated_at', 'deleted_at'];
 
@@ -14,10 +17,19 @@ class Login extends User
 
         $roleData = Role::find($this->role_id);
 
+        if($this->tokens()->count())
+            $this->tokens()->delete();
+
+        $token = $this->createToken('access_aplication')->plainTextToken;
+
         $this->role = [
             "id" => $roleData->id,
             "name" => $roleData->name,
-            "permissions" => $roleData->permissions($this->role_id)
+            "permissions" => $roleData->permissions($this->role_id),
+            "token" => [
+                "access_token" => $token,
+                "type" => "Bearer"
+            ]
         ];
         return $this;
     }
