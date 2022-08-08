ae3f8c5c3f2bf5597d72afb56de37f9aaae61d39
bmgcosta <bmgcosta@outlook.com>
2022-08-08 15:30:55 +0100

    Add new method to verify User Exists, Login and logout logic

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Login;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -29,21 +29,60 @@ class LoginController extends Controller
             }
 
             $userLogin = $this->verifUser($request);
-            
+
             return response()->json([
-                $userLogin->data(),
+                "access_token" => $userLogin->createToken('access_aplication')->plainTextToken,
+                "type" => "Bearer"
             ], 200);
         } catch (\Exception $exception) {
             return response()->json(['error' => $exception->getMessage()], $exception->getCode()) ;
         }        
     }
 
+    public function user(Request $request){
+        try {
+            $user = $request->user();
+            return response()->json($user->data(), 200);
+        } catch (\Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()], $exception->getCode());
+            return response()->json(['error' => $exception], 500);
+        }
+    }
+
+    public function logout(Request $request){
+        try {
+            $userFind = $this->findUserByEmail($request->user()->email);
+
+            $userFind->deleteToken($request);
+
+            return response()->json(["message" => "{$userFind->name}, See you later! Come back when you need!"], 200);
+
+        } catch (\Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()], $exception->getCode());
+        }
+    }
+
     private function verifUser(Request $request){
-        if((!Auth::attempt($request->only(['email', 'password']))))
-            throw new \Exception('User not found, please verify your email address and password!', 404);
-        $user = Login::where('email', $request->email)->first();
+
+        if (!Auth::attempt($request->only(['email', 'password']))) {
+            abort(403);
+        }
+        // if((!Auth::attempt($request->only(['email', 'password']))))
+        //     throw new \Exception('User not found, please check your email address and password!', 404);
+        $user = $this->findUserByEmail($request->email);
         if(!$user->active)
             throw new \Exception('User is disabled, please contact administrator or your manager!', 404);
         return $user; 
+    }
+
+    private function findUserByEmail($email){
+        try {
+            $userFound = Login::where('email', $email)->first();
+            if(!$userFound)
+                throw new \Exception('User not found, please check your email address!', 404);            
+            return $userFound;
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
     }
 }
