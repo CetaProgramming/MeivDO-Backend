666f56540a79b38b5ba1dad391ee6a018fa1aa0e
bmgcosta <bmgcosta@outlook.com>
2022-08-08 15:24:38 +0100

    PROJFINAL-8 Update route for working with auth:sactum

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::post('/login', 'LoginController@login');
+
+Route::middleware('auth:sanctum')->group(function (){
+    Route::get('/infouser', 'LoginController@user');
+    Route::post('/logout', 'LoginController@logout');
+    Route::get('/test', function(Request $request){
+        return response()->json('dsadas', 200);
+    });
+});
