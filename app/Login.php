2e3d7ad8551c41115105e806545d226d0375b6f7
bmgcosta <bmgcosta@outlook.com>
2022-08-09 17:47:43 +0100

    Remove delete token method

--- app/Login.php
+++ app/Login.php
@@ -4,12 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Role;
-use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Http\Request;
 
 class Login extends User
 {
-    use HasApiTokens;
 
     protected $table = 'users';
     protected $hidden = ['role_id', 'password', 'active', 'user_id', 'created_at', 'updated_at', 'deleted_at'];
@@ -23,16 +21,8 @@ class Login extends User
             "name" => $roleData->name,
             "permissions" => $roleData->permissions($this->role_id),
         ];
+        
         return $this;
-    }
-
-    public function deleteToken(Request $request){
-        try {
-            if($this->tokens()->count())
-                $request->user()->currentAccessToken()->delete();
-        } catch (\Exception $exception) {
-            return response()->json(['error' => $exception->getMessage()], $exception->getCode());
-        } 
     }
 
 }
