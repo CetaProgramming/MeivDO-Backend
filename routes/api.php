37cadead8bc8bc35c48d0705bc591f4320258350
BernardoTaveiraCarvalho <bernardo.taveira.carvalho@gmail.com>
2022-08-28 19:33:07 +0100

    PROJFINAL-32 Delete user

--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', 'LoginController@login');
 Route::get('/logout', 'LoginController@logout');
-Route::put('/users/{id}', 'UserController@update');
-Route::get('/users', 'UserController@index');
+
 Route::middleware(['auth:sanctum'])->group(function (){
     Route::get('/user', 'LoginController@user');
-
+    Route::get('/user', 'LoginController@user');
+    Route::delete('/users/{id}', 'UserController@destroy');
+    Route::put('/users/{id}', 'UserController@update');
+    Route::get('/users', 'UserController@index');
     Route::get('/roles','RoleController@index');
 });
