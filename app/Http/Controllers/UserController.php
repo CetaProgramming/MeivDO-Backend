0e1e1dcc6194d3bf6031c85a5353f635bae1d84b
BernardoTaveiraCarvalho <bernardo.taveira.carvalho@gmail.com>
2022-08-28 15:20:10 +0100

    PROJFINAL-29 Get User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,18 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Role;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
-
-class RoleController extends Controller
+use Illuminate\Support\Facades\DB;
+class UserController extends Controller
 {
     public function index()
     {
         $request =Auth::user();
         try {
-           Log::info("User with email {$request->email} started a new session");
-            return response()->json(Role::all(), 200);
+            Log::info("User with email {$request->email} started a new session");
+            return response()->json(DB::table('users')->paginate(15), 200);
         } catch (\Exception $exception) {
             Log::error("Try access with email {$request->email} but not is possible!");
             return response()->json(['error' => $exception], 500);
