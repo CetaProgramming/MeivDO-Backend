f51b489490328de9aee7bf1995a6c7ca140f79b2
BernardoTaveiraCarvalho <bernardo.taveira.carvalho@gmail.com>
2022-09-21 18:59:23 +0100

    PROJFINAL-122 Delete Projects

--- app/Project.php
+++ app/Project.php
@@ -3,13 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Project extends Model
 {
+   // protected $fillable = ['name','active'];
+    use SoftDeletes;
     public function projectTools(){
         return $this->hasMany('App\ProjectTool');
     }
     public function user(){
         return $this->belongsTo('App\User');
+    }
+    protected static function booted()
+    {
+        static::deleting(function ($goalType) {
+            if ($goalType->projectTools()->exists()) {
+                throw new \Exception("The project have relations", 500);
+            }
+        });
     }
 }
