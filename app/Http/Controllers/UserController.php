4cc110a7bd42476b503e7e2ef9e261b4ef6d8749
bmgcosta <bmgcosta@outlook.com>
2022-09-26 21:46:07 +0100

    PROJFINAL-166 Fix users search

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -36,10 +36,16 @@ class UserController extends Controller
         $Auth=Auth::user();
         try {
             Log::info("User with email { $Auth->email} made a search on table users");
-            return response()->json(User::where("email", "LIKE", "%{$request->name}%")
-            ->where("active", "LIKE", "%{$request->active}%")
-            ->orWhere("name", "LIKE", "%{$request->name}%")
-            ->with(['role'])->paginate(), 200);
+            return response()->json(User::
+                where([
+                    ["email", "LIKE", "%{$request->name}%"],
+                    ["active", "LIKE", "%{$request->active}%"]
+                ])
+                ->orWhere([
+                    ["name", "LIKE", "%{$request->name}%"],
+                    ["active", "LIKE", "%{$request->active}%"]
+                ])
+                ->with(['role'])->paginate(), 200);
         } catch (\Exception $exception) {
             Log::error("User with email { $Auth->email} receive an error on search users( {$exception->getMessage()})");
             return response()->json(['error' => $exception->getMessage()], $exception->getCode());
