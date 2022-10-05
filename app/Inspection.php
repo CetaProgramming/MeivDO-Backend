540edde711402a51f05a8e3253678720ffe79090
bmgcosta <bmgcosta@outlook.com>
2022-10-05 19:26:38 +0100

    PROJFINAL-158 Build relationship get all tools associate on that inspection

--- app/Inspection.php
+++ app/Inspection.php
@@ -4,19 +4,19 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
 
 class Inspection extends Model
 {
     protected $fillable = ['additionalDescription','status','user_id'];
     use SoftDeletes;
 
-   // public function tool(){
-   //     return $this->belongsTo('App\Tool',"inspectionTool");
-   // }
     public function user(){
         return $this->belongsTo('App\User');
     }
+
     public function inspectionTool(){
-        return $this->belongsToMany('App\Inspection_Tool','inspection_tool');
+        return DB::table('inspection_tool')
+        ->where('inspection_id', '=', $this->id)->get();
     }
 }
