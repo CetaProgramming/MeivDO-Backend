c8bfc07724582c8d942c27e04dc481aedde5ea73
BernardoTaveiraCarvalho <bernardo.taveira.carvalho@gmail.com>
2022-10-18 18:06:41 +0100

    PROJFINAL-243 Repartion update on inspection update change model

--- app/Reparation.php
+++ app/Reparation.php
@@ -20,18 +20,4 @@ class Reparation extends Model
         $this->status=0;
         $this->save();
     }
-    public  function updateReparation($inspection,$request,$Auth){
-        if($this != null) {
-            if ($this->status == 1) {
-                throw new \Exception("Inspection with id: {$inspection->id} cannot be updated because reparation is finish", 500);
-            }else{
-                if($request->status == 0){
-                    $reparation = new Reparation();
-                    $reparation->createReparation($Auth,$inspection->id);
-                }else{
-                    $this->delete();
-                }
-            }
-        }
-    }
 }
