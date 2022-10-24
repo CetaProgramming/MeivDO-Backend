f47e0484a2abb2a5d8ed76516dac07d79624a410
bmgcosta <bmgcosta@outlook.com>
2022-10-24 19:56:58 +0100

    PROJFINAL-279 Remove routes to register new user

--- routes/web.php
+++ routes/web.php
@@ -23,18 +23,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('/', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
-
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
